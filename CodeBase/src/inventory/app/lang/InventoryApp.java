184765f447381ddb88dfce32b8a65deedb782190
Darian Culver <culver.darian@gmail.com>
2021-04-05 21:38:08 +1200

    File Structure and main func
    -Set git up
    -Made main function and main package

---
+++ CodeBase/src/inventory/app/lang/InventoryApp.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package inventory.app.lang;
+
+/**
+ * @author darianisak
+ *
+ */
+public class InventoryApp {
+
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
